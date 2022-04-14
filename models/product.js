2b58180ca1494fc1c75a4e4fb93a676ff2cbb6bb
luis jesus villegas castillo <lc77278@gmail.com>
2022-04-14 13:27:44 -0500

    linter product

--- models/product.js
+++ models/product.js
@@ -1,50 +1,49 @@
 const { Schema, model } = require('mongoose');
 const mongoosePaginate = require('mongoose-paginate-v2');
-const ProductSchema = Schema({
 
+const ProductSchema = Schema({
   nombre: {
     type: String,
-    required: [ true, 'El campo es requerido'],
-    unique: true
+    required: [true, 'El campo es requerido'],
+    unique: true,
   },
   estado: {
     type: Boolean,
     default: true,
-    required: true
+    required: true,
   },
   usuario: {
     type: Schema.Types.ObjectId,
     ref: 'Usuario',
-    required: true
+    required: true,
   },
   precio: {
     type: Number,
-    default: 0
+    default: 0,
   },
   categoria: {
     type: Schema.Types.ObjectId,
     ref: 'Categoria',
-    required: true
+    required: true,
   },
   descripcion: {
-    type: String
+    type: String,
   },
   disponible: {
     type: Boolean,
-    default: true
+    default: true,
   },
   img: {
-    type: String
-  }
-
+    type: String,
+  },
 });
 
 ProductSchema.set('toJSON', {
-  transform: (document, object)=>{
+  transform: (document, object) => {
     object.id = object._id;
     delete object._id;
     delete object.__v;
-  }
+  },
 });
 
 ProductSchema.plugin(mongoosePaginate);
